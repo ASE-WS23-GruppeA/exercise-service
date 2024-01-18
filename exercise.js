85187282f5889662b694c9af51e02e79731cf79f
mariniou77 <antonis.marinidis7@gmail.com>
2024-01-18 10:40:47 +0100

    renaming on exercises.js

--- exercise.js
+++ exercise.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const actions = require("./exerciseService");
+const exerciseServise = require("./exerciseService");
 
 const app = express();
 const port = process.env.PORT;
@@ -8,11 +8,11 @@ app.use(express.json());
 
 // Route to get all the exercises
 // Use the function as the handler for the '/exercises' route
-app.get('/exercises', actions.getExercisesHandler);
+app.get('/exercises', exerciseServise.getExercisesHandler);
 
 // Route to get a specific exercise by ID
 // Use the function as the handler for the '/exercises/:id' route
-app.get('/exercises/:id', actions.getExerciseByIdHandler);
+app.get('/exercises/:id', exerciseServise.getExerciseByIdHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
