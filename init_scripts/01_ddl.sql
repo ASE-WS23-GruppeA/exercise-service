a5c646e7a52ee4927234208ad758187533f32d15
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2024-11-12 15:24:44 +0100

    Remove docker image push; move data insertion from JS to SQL

---
+++ init_scripts/01_ddl.sql
@@ -0,0 +1,122 @@
+CREATE TABLE exercises (
+  "exerciseID"   SERIAL NOT NULL,
+  "exerciseName" TEXT   NOT NULL,
+  "muscleGroup"  TEXT   NOT NULL,
+
+  CONSTRAINT exercises_pkey PRIMARY KEY ("exerciseID")
+);
+
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Squats', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Push-ups', 'Chest');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Lunges', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Pull-ups', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Planks', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Deadlifts', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Bench Press', 'Chest');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Sit-ups', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Leg Press', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Dips', 'Chest');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Russian Twists', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Calf Raises', 'Calves');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Barbell Rows', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Leg Curls', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Shoulder Press', 'Shoulders');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Hanging Leg Raise', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Pull-downs', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Leg Extensions', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Bicep Curls', 'Biceps');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Tricep Extensions', 'Triceps');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Bicycle Crunches', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Hammer Curls', 'Biceps');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Lateral Raises', 'Shoulders');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Seated Rows', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Hip Thrusts', 'Glutes');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Incline Bench Press', 'Chest');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Reverse Flyes', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Romanian Deadlifts', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Ab Wheel Rollouts', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Cable Curls', 'Biceps');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Skull Crushers', 'Triceps');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Flutter Kicks', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Dumbbell Rows', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Front Raises', 'Shoulders');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Step-ups', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Cable Pull-throughs', 'Glutes');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Decline Bench Press', 'Chest');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Face Pulls', 'Shoulders');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Stiff-legged Deadlifts', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Dragon Flags', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Cable Rows', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Sissy Squats', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Preacher Curls', 'Biceps');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Close-grip Bench Press', 'Triceps');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Reverse Crunches', 'Core');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Lat Pulldowns', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Barbell Shrugs', 'Shoulders');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Hack Squats', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Hamstring Curls', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Lunges with Dumbbells', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Dumbbell Bench Press', 'Chest');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Dumbbell Flyes', 'Chest');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Chin-ups', 'Back');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Box Jumps', 'Legs');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Kettlebell Swings', 'Glutes');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Woodchoppers', 'Obliques');
+INSERT INTO exercises ("exerciseName", "muscleGroup")
+VALUES ('Leg Raises', 'Core');
